5f976445af699ed8a7df5b55b9a772e8e9b1e712
add-bake <1872534530@qq.com>
2019-01-28 11:40:17 +0800

    页面优化

--- client/src/components/FaceRecognition.vue
+++ client/src/components/FaceRecognition.vue
@@ -7,7 +7,13 @@
       playsinline>
     </video>
     <!--描绘video截图-->
-    <div class="modal"></div>
+    <div class="modal">
+      <p
+        v-if="testing"
+        class="testing">
+          检测中<span class="dotting"></span>
+        </p>
+    </div>
     <canvas
       id="canvas"
       class="canvas">
@@ -23,7 +29,9 @@ export default {
   name: 'FaceRecognition',
   data () {
     return {
-      clientHeight: `${document.documentElement.clientHeight}px`
+      video: '',
+      clientHeight: `${document.documentElement.clientHeight}px`,
+      testing: true
     }
   },
   mounted () {
@@ -55,43 +63,48 @@ export default {
         navigator.getUserMedia(constraints, success, error)
       }
     },
-    success (stream) {
-      let video = document.getElementById('video') 
+    async success (stream) {
       //兼容webkit核心浏览器
+      this.video = document.getElementById('video')
       //将视频流转化为video的源
-      video.srcObject = stream
+      await this.$nextTick()
+      this.video.srcObject = stream
       // video.play()//播放视频
       //将视频绘制到canvas上
-      this.postFace(video)
+      this.postFace()
     },
     error (error) {
       console.log('访问用户媒体失败：', error.name, error.message)
     },
-    postFace (video) {
+    postFace () {
+      this.video.addEventListener('canplay', () => {
+        this.sendAuth()
+      });
+    },
+    sendAuth () {
       let scale = 0.4
       let canvas = document.getElementById('canvas')
       let context = canvas.getContext('2d')
-      video.addEventListener('canplay', () => {
-        canvas.width = video.videoWidth * scale
-        canvas.height = video.videoHeight * scale
-        setTimeout(() => {
-          context.drawImage(video, 0, 0, canvas.width, canvas.height)
-          let img = canvas.toDataURL('image/jpg')
-          
-          this.sendAuth(img.split(',')[1]) // 获取完整的base64编码
-        }, 3000)
-      });
-    },
-    sendAuth (src) {
+      
+      this.testing = true      
+      canvas.width = this.video.videoWidth * scale
+      canvas.height = this.video.videoHeight * scale
+      context.drawImage(this.video, 0, 0, canvas.width, canvas.height)
+      let img = canvas.toDataURL('image/jpg') // 获取完整的base64编码
+
       this.$axios.post('/auth', {
-        src: src
+        src: img.split(',')[1]
       })
-      .then(res => {
+      .then(async res => {
+        this.testing = false
+        await this.$nextTick()
         let {data} = res
         console.log(data)
         alert(data.code === 0 ? data.result.score >= 80 ? '检测通过' : '检测不通过' : errorTips(data.code) || '人脸识别未知错误')
+        if (data.code !== 0 || data.result && data.result.score < 80) this.sendAuth()
       })
       .catch(err => {
+        this.testing = false
         console.log(err)
       })
     }
@@ -116,7 +129,29 @@ export default {
   background: url("../assets/modal_face_auth.png") 0 0 no-repeat;
   background-size: cover;
 }
+.modal .testing {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 20px;
+  z-index: 1;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+}
+.dotting {
+  display: inline-block;
+  min-width: 2px;
+  min-height: 2px;
+  box-shadow: 2px 0 currentColor, 6px 0 currentColor, 10px 0 currentColor; 
+  animation: dot 4s infinite step-start both;
+}
 .canvas {
   display: none;
+}
+@keyframes dot {
+    25% { box-shadow: none; }
+    50% { box-shadow: 2px 0 currentColor; }
+    75% { box-shadow: 2px 0 currentColor, 6px 0 currentColor; }
 }
 </style>
